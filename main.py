1c18e13b81225e1ba48f49c429adbb67ca102d85
Łukasz <109048586+zwozlukasz@users.noreply.github.com>
2023-01-20 00:45:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+# string
+print("karuzela"[5])
+print(f"dlugosc stringu karuzela: {len('karuzela')}")
+print("string 1 " + "string 2")
+print(" To jest miejsce na ciag pierwszy %s i drugi %s i ilczby %4d"%("ala", "kot", 34))
+print("ciag drugi {} i znowu cos {}".format("aaaa", 343344))
+
+# int, float
+print(len(str(2323.444)))
+# jak widac powyzej znak kropki jest traktowany jako osobny znak
+
+# Boolean
+True
+False
+
+print("Hello world from ...")
+os.system("python --version")
+
